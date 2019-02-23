a79ba711947d7bde106408c9ca3161abd038b74d
tslight <tobyslight@gmail.com>
2019-02-23 22:35:18 +0000

    makes more sense to have these here

---
+++ Public/ConvertTo-HashTable.ps1
@@ -0,0 +1,18 @@
+function ConvertTo-HashTable {
+    [CmdletBinding(SupportsShouldProcess)]
+    Param (
+	[Parameter(Mandatory,ValueFromPipeline)]
+	[AllowNull()]
+	[object]$Object
+    )
+
+    process {
+	$HashTable = @{}
+
+	foreach ($Property in $Object.PSObject.Properties.Name) {
+	    $HashTable[$Property] = $Object.$Property
+	}
+
+	return $HashTable
+    }
+}

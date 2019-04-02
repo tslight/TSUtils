614c1c79ccfb459868738e75296003e583d6863e
tslight <tobyslight@gmail.com>
2019-04-02 18:01:59 +0100

    lazygit

--- Public/Merge-Objects.ps1
+++ Public/Merge-Objects.ps1
@@ -3,21 +3,35 @@ function Merge-Objects {
     Param (
 	[Parameter(Mandatory)]
 	[AllowNull()]
-	[object[]]$ObjectOne,
+	[object]$ObjectOne,
 	[Parameter(Mandatory)]
 	[AllowNull()]
-	[object[]]$ObjectTwo
+	[object]$ObjectTwo,
+	[AllowEmptyString()]
+	[string]$PrefixOne,
+	[AllowEmptyString()]
+	[string]$PrefixTwo
     )
 
-    $Properties = [PSCustomObject]@()
+    $Properties = [ordered]@{}
 
-    foreach ($Property in $Object1.PSObject.Properties) {
-	$Properties += @{$Property.Name = $Property.Value}
+    foreach ($Property in $ObjectOne.PSObject.Properties) {
+	if ($Property.Value -ne $Null) {
+	    $Properties.Add("$PrefixOne$($Property.Name)", $Property.Value)
+	}
     }
 
-    foreach ($Property in $Object1.PSObject.Properties) {
-	$Properties += @{$Property.Name = $Property.Value}
+    foreach ($Property in $ObjectTwo.PSObject.Properties) {
+	if ($Property.Value -ne $Null) {
+	    try {
+		$Properties.Add("$PrefixTwo$($Property.Name)", $Property.Value)
+	    } catch [System.Management.Automation.MethodInvocationException] {
+		Write-Warning "Duplicate property name. Adding underscore to second."
+		Write-Warning "To avoid this use the prefix parameters."
+		$Properties.Add("$($Property.Name)_", $Property.Value)
+	    }
+	}
     }
 
-    [PSCustomObject]$Properties
+    New-Object PSObject -Property $Properties
 }

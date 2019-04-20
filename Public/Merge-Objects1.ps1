b7a5ef995e54386ca50d0b6f6553339ccb1861ba
tslight <tobyslight@gmail.com>
2019-04-20 13:01:46 +0100

    support both pipelining and traditional argparse

--- Public/Merge-Objects1.ps1
+++ Public/Merge-Objects1.ps1
@@ -2,27 +2,28 @@ function Merge-Objects1 {
     [CmdletBinding()]
     Param (
 	[Parameter(Mandatory,ValueFromPipeline)]
-	[object[]]$Object
+	[object[]]$Objects
     )
 
     begin {
 	$Properties = [ordered]@{}
-	$Count = 0
     }
 
     process {
-	foreach ($Property in $Object.PSObject.Properties) {
-	    if ($Property.Value -ne $Null) {
-		try {
-		    $Properties.Add("$($Property.Name)", $Property.Value)
-		} catch [System.Management.Automation.MethodInvocationException] {
-		    Write-Warning "Duplicate property name. Adding underscore to second."
-		    Write-Warning "To avoid this use the prefix parameters."
-		    $Properties.Add("$($Property.Name)$Count", $Property.Value)
+	foreach ($Object in $Objects) {
+	$Count = 1
+	    foreach ($Property in $Object.PSObject.Properties) {
+		if ($Property.Value -ne $Null) {
+		    try {
+			$Properties.Add("$($Property.Name)", $Property.Value)
+		    } catch [System.Management.Automation.MethodInvocationException] {
+			Write-Verbose "Duplicate property name. Adding $Count counter."
+			$Properties.Add("$($Property.Name)-$Count", $Property.Value)
+		    }
 		}
 	    }
-	}
 	$Count++
+	}
     }
 
     end {

503a934ae6742e26a10771a61884ec64644b413e
tslight <tobyslight@gmail.com>
2019-04-24 13:01:49 +0100

    changed add-defaultdisplayset to not make typename mandatory

--- Public/Add-DefaultDisplaySet.ps1
+++ Public/Add-DefaultDisplaySet.ps1
@@ -4,20 +4,21 @@ function Add-DefaultDisplaySet {
 	[Parameter(Mandatory,ValueFromPipeline)]
 	[object]$Object,
 	[Parameter(Mandatory)]
-	[string]$TypeName, # Give this object a unique typename
-	[Parameter(Mandatory)]
-	[array]$DefaultDisplaySet # Configure a default display set
+	[array]$DefaultDisplaySet, # Configure a default display set
+	[string]$TypeName # Give this object a unique typename
     )
 
     process {
 	# https://learn-powershell.net/2013/08/03/quick-hits-set-the-default-property-display-in-powershell-on-custom-objects/
-	$Object.PSObject.TypeNames.Insert(0,$TypeName)
+	if ($TypeName) {
+	    $Object.PSObject.TypeNames.Insert(0,$TypeName)
+	}
 	# Create the default property display set
 	$DefaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(
 	    ‘DefaultDisplayPropertySet’,[string[]]$DefaultDisplaySet
 	)
 	$PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($DefaultDisplayPropertySet)
 	$Object | Add-Member MemberSet PSStandardMembers $PSStandardMembers
-	Write-output $Object
+	Write-Output $Object
     }
 }

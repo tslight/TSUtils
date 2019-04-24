024c6985f182275464bd99a9cbf5952edea93fcb
tslight <tobyslight@gmail.com>
2019-04-24 23:57:24 +0100

    random functions in own module now

--- TSUtils.psm1
+++ TSUtils.psm1
@@ -1,22 +1,42 @@
 #region get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$Public  = @(
+    Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue
+)
+$Private = @(
+    Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue
+)
 #endregion
 
 #region source the files
-foreach ($import in @($Public + $Private)) {
-    Try {
-	. $import.fullname
-    } Catch {
-	Write-Error -Message "Failed to import function $($import.fullname): $_"
+foreach ($Function in @($Public + $Private)) {
+    $FunctionPath = $Function.fullname
+    try {
+	. $FunctionPath # dot source function
+    } catch {
+	Write-Error -Message "Failed to import function at $(FunctionPath): $_"
     }
 }
 #endregion
 
 #region read in or create an initial config file and variable
+
 #endregion
 
 #region set variables visible to the module and its functions only
+$Date = Get-Date -UFormat "%Y.%m.%d"
+$Time = Get-Date -UFormat "%H:%M:%S"
+$Green = @{
+    Background = 'Black'
+    Foreground = 'Green'
+}
+$Cyan = @{
+    Background = 'Black'
+    Foreground = 'Cyan'
+}
+$Magenta = @{
+    Background = 'Black'
+    Foreground = 'Magenta'
+}
 #endregion
 
 #region export Public functions ($Public.BaseName) for WIP modules

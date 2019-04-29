a8f09d8580ec4844ce9c9993eea38c321b65c5f5
tslight <tobyslight@gmail.com>
2019-04-29 14:48:20 +0100

    full flesh out module template generation

--- Public/New-ModuleTemplate.ps1
+++ Public/New-ModuleTemplate.ps1
@@ -3,13 +3,33 @@ function New-ModuleTemplate {
     Param (
 	[Parameter(Mandatory,ValueFromPipeline)]
 	[string[]]$Name,
-	[System.IO.FileInfo]$Path=($env:PSModulePath -split ':|;' | Select-Object -last 1)
+	[System.IO.FileInfo]$Path=($env:PSModulePath -split ':|;' | Select-Object -last 1),
+	[switch]$Config
     )
 
+    begin {
+	$ModuleTemplateDir = "$PSScriptRoot\..\Templates"
+	$ModuleTemplateWithConfig = "$ModuleTemplateDir\Template-Config.psm1"
+	$ModuleTemplate = "$ModuleTemplateDir\Template.psm1"
+	$ModuleManifest = "$ModuleTemplateDir\Template.psd1"
+    }
+
     process {
-	New-Path "$Path\$Name\Public" -Type 'Directory'
-	New-Path "$Path\$Name\Private" -Type 'Directory'
-	New-Path "$Path\$Name\$Name.psm1" -Type 'File'
-	New-Path "$Path\$Name\$Name.psd1" -Type 'File'
+	$ModuleDirectories = @(
+	    'Classes'
+	    'Public'
+	    'Private'
+	)
+
+	foreach ($Directory in $ModuleDirectories) {
+	    New-Path "$Path\$Name\$Directory" -Type 'Directory'
+	}
+
+	if ($Config) {
+	    Get-Content $ModuleTemplateWithConfig | Set-Content "$Path\$Name\$Name.psm1"
+	} else {
+	    Get-Content $ModuleTemplate | Set-Content "$Path\$Name\$Name.psm1"
+	}
+	Get-Content $ModuleManifest | Set-Content "$Path\$Name\$Name.psd1"
     }
 }
--- Templates/Template-Config.psm1
+++ Templates/Template-Config.psm1
@@ -19,6 +19,36 @@ foreach ($Function in @($Public + $Private)) {
 #endregion
 
 #region read in or create an initial config file and variable
+$ConfigFile = "Config.psd1"
+$Params     = @{
+    BaseDirectory = $PSScriptRoot
+    FileName      = $ConfigFile
+}
+
+if (Test-Path "$PSScriptRoot\$ConfigFile") {
+    try {
+	$Config = Import-LocalizedData @Params
+	$Config = Import-LocalizedData @Params
+	foreach ($variable in $Config.keys) {
+	    Write-Verbose "Setting $var variable."
+	    New-Variable -Name "$variable" -Value $Config.$variable -Force
+	}
+    } catch {
+	Write-Warning "Invalid configuration data in $ConfigFile."
+	Write-Warning "Please fill out or correct $PSScriptRoot\$ConfigFile."
+	Write-Verbose $_.Exception.Message
+	Write-Verbose $_.InvocationInfo.ScriptName
+	Write-Verbose $_.InvocationInfo.PositionMessage
+    }
+} else {
+    @"
+@{
+    Variable = ""
+}
+"@ | Out-File -Encoding UTF8 -FilePath "$PSScriptRoot\$ConfigFile"
+    Write-Warning "Generated $PSScriptRoot\$ConfigFile."
+    Write-Warning "Please edit $ConfigFile and re-import module."
+}
 #endregion
 
 #region set variables visible to the module and its functions only

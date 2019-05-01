d2d9d88cbde3675fcd69f85ecd0ed9392ef87655
tslight <tobyslight@gmail.com>
2019-05-01 16:47:27 +0100

    use new-modulemanifest instead of re-inventing the wheel

--- Public/New-ModuleTemplate.ps1
+++ Public/New-ModuleTemplate.ps1
@@ -1,45 +1,51 @@
 function New-ModuleTemplate {
     <#
-.SYNOPSIS
-Generate module scaffolding and boilerplate.
+    .SYNOPSIS
+    Generate module scaffolding and boilerplate.
 
-.DESCRIPTION
-Creates Public, Private & Classes directories. A psm1 script that sources
-functions from Public & Private and exports those in Public, a manifest
-file, a file that defines color splatting hash tables and a script for
-supporting module configuration management.
+    .DESCRIPTION
+    Creates Public, Private & Classes directories. A psm1 script that sources
+    functions from Public & Private and exports those in Public, a manifest
+    file, a file that defines color splatting hash tables and a script for
+    supporting module configuration management.
 
-.PARAMETER Names
-The names of the modules you want to create.
+    .PARAMETER Names
+    The names of the modules you want to create.
 
-.PARAMETER Path
-The path to store these modules in. Defaults to the last path found in
-$env:PSMOdulePath
+    .PARAMETER Path
+    The path to store these modules in. Defaults to the last path found in
+    $env:PSMOdulePath
 
-.PARAMETER Author
-Name of the modules author. This will be inserted into the Author and
-Copyright fields in the manifest file. Defaults to the current users' name
+    .PARAMETER Author
+    Name of the modules author. This will be inserted into the Author and
+    Copyright fields in the manifest file. Defaults to the current users' name
 
-.PARAMETER UncommentConfig
-A switch that uncomments a line in the psm1 file that dot sources the
-configuration management script. This is stored in Config.ps1 and allows
-reading in of sensitive data stored in a Config.psd1 for setting variables
-available in the modules' scope.
+    .PARAMETER UncommentConfig
+    A switch that uncomments a line in the psm1 file that dot sources the
+    configuration management script. This is stored in Config.ps1 and allows
+    reading in of sensitive data stored in a Config.psd1 for setting variables
+    available in the modules' scope.
 
-.EXAMPLE
-New-ModuleTemplate -Names Module1,Module2,Module3 -Path C:\MyModules -Author 'Me' -UncommentConfig
-
-The example above will create Module1, Module2 & Module3 under
-C:\MyModules. Author and Copyright fields in the modules manifest file will
-be set to 'Me'. The psm1 will dot source the Config.ps1 script that handles
-reading in a configuration psd1 file.
-#>
+    .EXAMPLE
+    $Params = @{
+	Name = Module1,Module2,Module3
+	Path = "C:\MyModules"
+	Author = "Me Myself & I"
+	CompanyName = "My Awesome Company"
+	Description = "This module will rock your world!"
+	RequiredModules = 'All','My','Other','Modules'
+    }
+    New-ModuleTemplate @Params
+    #>
     [CmdletBinding()]
     Param (
 	[Parameter(Mandatory,ValueFromPipeline)]
 	[string[]]$Names,
 	[System.IO.FileInfo]$Path=($env:PSModulePath -split ':|;' | Select-Object -last 1),
 	[string]$Author=$env:USER,
+	[string]$CompanyName="",
+	[string]$Description="Module Description",
+	[string[]]$RequiredModules=@(),
 	[switch]$UncommentConfig
     )
 
@@ -81,9 +87,21 @@ reading in a configuration psd1 file.
 	    Copy-Item $Colors "$Path\$Name\Colors.ps1"
 	    Write-Verbose "Copied $Colors to $Path\$Name\Colors.ps1."
 
-	    (Get-Content $Manifest) -Replace('ModuleName', $Name) -Replace('AuthorName', $Author) |
-	      Set-Content "$Path\$Name\$Name.psd1"
-	    Write-Verbose "Copied $Manifest to $Path\$Name\$Name.psd1."
+	    $Params = @{
+		Path = "$Path\$Name\$Name.psd1"
+		RootModule = $Name
+		Author = $Author
+		Copyright = "(c) $(Get-Date -Uformat %Y) $Author. All rights reserved."
+		CompanyName = $CompanyName
+		Description = $Description
+		RequiredModules = $RequiredModules
+		FunctionsToExport = '*'
+		AliasesToExport = '*'
+		VariablesToExport = '*'
+		CmdletsToExport = '*'
+	    }
+	    New-ModuleManifest @Params
+	    Write-Verbose "Generated $Module manifest at $Path\$Name\$Name.psd1."
 
 	    Copy-Item $GitIgnore "$Path\$Name\.gitignore"
 	    Write-Verbose "Copied $GitIgnore to $Path\$Name\.gitignore."
